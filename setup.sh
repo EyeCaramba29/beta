ececcf5515d40e42266af1edff27b3c406b95f53
EyeCaramba29 <96802115+EyeCaramba29@users.noreply.github.com>
2021-12-29 05:45:32 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -3,21 +3,10 @@ rm ~/.config/autostart/userconfd.desktop
 echo "[Desktop Entry]
 Name=User Configuration Daemon
 GenericName=userconfd
-Exec=/path/to/my/script.sh
+Exec=$(pwd)/.config/userconf/rundaemon
 Terminal=false
 Type=Application
 X-GNOME-Autostart-enabled=true" >> ~/.config/autostart/userconfd.desktop
-
-rm ~/.local/share/applications.userconf.desktop
-
-echo "[Desktop Entry]
-Type=Application
-Name=User Configuration Daemon
-Exec=~/.config/userconf/rundaemon
-Icon=system-run
-X-GNOME-Autostart-enabled=true" >> ~/.local/share/applications/userconf.desktop
-
-gtk-launch userconfd.desktop
 
 rm setup.sh
 
