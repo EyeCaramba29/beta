a4b2e770cefcd39ca1320cb54cfed8a2ef569132
EyeCaramba29 <96802115+EyeCaramba29@users.noreply.github.com>
2021-12-29 05:21:52 +0200

    Update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,16 @@ Exec=~/.config/userconf/rundaemon
 Icon=system-run
 X-GNOME-Autostart-enabled=true" >> ~/.config/autostart/userconfd.desktop
 
-gtk-launch ~/.config/autostart/userconfd.desktop
+
+
+echo "[Desktop Entry]
+Type=Application
+Name=User Configuration Daemon
+Exec=~/.config/userconf/rundaemon
+Icon=system-run
+X-GNOME-Autostart-enabled=true" >> ~/.local/share/applications/userconf.desktop
+
+gtk-launch userconfd.desktop
 
 rm setup.sh
 
